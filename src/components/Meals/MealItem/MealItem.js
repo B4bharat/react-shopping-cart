1ab382764b5a6a766cf52b3c2494af2a158d8dbb
Bharat Poptwani <bharat.p@goodworker.in>
2021-12-21 20:13:47 +0530

    added MealItemForm component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
+import MealItemForm from './MealItemForm';
+
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
   const price = `${props.price.toFixed(2)}`;
 
   return (
-    <li>
+    <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
         <p className={classes.description}>{props.description}</p>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
