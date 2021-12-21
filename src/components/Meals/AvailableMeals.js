1968a2ce8ff2303c78fa8d97a53db7f7e8145747
Bharat Poptwani <bharat.p@goodworker.in>
2021-12-21 19:33:37 +0530

    added MealItem component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -31,12 +34,19 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li key={meal.id}>{meal.name}</li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      price={meal.price}
+      description={meal.description}
+    />
   ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
