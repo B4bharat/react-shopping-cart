26258c136e86ac72edd1625829c1fd1aafce76a6
Bharat Poptwani <bharat.p@goodworker.in>
2021-12-22 21:08:10 +0530

    added cartReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 
+// ---------------- Components ----------------------
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+
+// ---------------- State Management --------------------
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cart, setCart] = useState(false);
@@ -16,13 +20,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cart && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
